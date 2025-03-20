9ea549bafab8d0a9c24abd4e9e3b89f037c918c0
Gabriel Martins <gabrielmartins23445@gmail.com>
2025-03-20 10:35:20 -0300

    criei a tela de login

--- Front/src/components/FormRegister/index.jsx
+++ Front/src/components/FormRegister/index.jsx
@@ -6,12 +6,12 @@ function FormRegister() {
         <div id='divCadastro'>
             <img className='Logo' src={Logo} alt="" />
             <form id='formCadastro'>
-                <h1>Crie uma Conta</h1>
+                <h1 >Crie uma Conta</h1>
                 <input className='input' type="text" name='nome' placeholder='Digite seu nome' required />
 
                 <input className='input' type="text" name='idade' placeholder='Digite sua idade' required />
 
-                <input className='input' type="email" name='email' placeholder='Digite sua email' required />
+                <input className='input' type="email" name='email' placeholder='Digite seu email' required />
 
                 <input className='input' type="password" name='senha' placeholder='Digite sua senha' required />
 
--- Front/src/components/Login/index.jsx
+++ Front/src/components/Login/index.jsx
@@ -1,8 +1,19 @@
 import '../Login/login.css'
+import Logo from '/coin.png'
 
 function Login() {
     return (
-        <h1>aqui vai ser o login</h1>
+        <div id='divLogin'>
+            <img className='imgLogin' src={Logo} alt="" />
+            <form id='formLogin'>
+                <h1>Login</h1>
+                <input className='inputLogin' type="text" name='nome' placeholder='Usuário' required />
+
+                <input className='inputLogin' type="password" name='senha' placeholder='Senha' required />
+
+                <button id='botaoLogin' type='submit'>Entrar</button>
+            </form>
+        </div>
     );
 }
 
